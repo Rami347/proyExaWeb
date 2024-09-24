61de409eb65ef6cc96df4db533253a7fab782cf2
KendCode <kenderacademico@gmail.com>
2024-09-24 11:13:57 -0400

    modifico ventas mostrando nombre de cliente

--- ventas.php
+++ ventas.php
@@ -56,7 +56,14 @@
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                         </button>
-                        <h2>hola mundo </h2>
+                        <?php
+                			session_start(); // Asegúrate de tener esto al inicio
+							   if (isset($_SESSION['cli'])) {
+							   	echo "<h1 class='display-4 fw-bolder'>".$_SESSION['cli']."</h1>";
+							   } else {
+							   	echo "<h1 class='display-4 fw-bolder'>No hay cliente registrado</h1>";
+							   }
+                		?>
                      </nav>
                   </div>
                </div>
